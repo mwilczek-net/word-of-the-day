18f7c6acfeb3c52a9c2db7fe540e845e1bb40b81
mwilczek-net <mwilczek-net@users.noreply.github.com>
2023-09-22 10:58:25 +0200

    Basic configuration

---
+++ word-of-the-day.sh
@@ -0,0 +1,10 @@
+#!/bin/zsh
+
+CURRENT_PATH="$(cd "$(dirname "$0")" && pwd -P)"
+WORDS_PATH="${CURRENT_PATH}/words"
+
+file=$(ls "${WORDS_PATH}" | sort -R | tail -n1)
+
+echo '--------------========|       WORD  OF  THE  DAY       |========--------------'
+cat "${WORDS_PATH}/${file}" | toilet -t -f term --filter border
+echo '--------------========|       WORD  OF  THE  DAY       |========--------------'
