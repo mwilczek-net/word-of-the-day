1f04431ca6b73d4506c9905adcfa0f6390d6e7f6
mwilczek-net <mwilczek-net@users.noreply.github.com>
2023-09-22 11:07:40 +0200

    Format text

--- word-of-the-day.sh
+++ word-of-the-day.sh
@@ -6,5 +6,6 @@ WORDS_PATH="${CURRENT_PATH}/words"
 file=$(ls "${WORDS_PATH}" | sort -R | tail -n1)
 
 echo '--------------========|       WORD  OF  THE  DAY       |========--------------'
-cat "${WORDS_PATH}/${file}" | toilet -t -f term --filter border
+cat "${WORDS_PATH}/${file}"
+echo
 echo '--------------========|       WORD  OF  THE  DAY       |========--------------'
