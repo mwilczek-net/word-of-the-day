800704184c8ffd4e9ad7ee00d141f78df77bbadd
mwilczek-net <mwilczek-net@users.noreply.github.com>
2023-09-29 12:43:04 +0200

    Reorganise language files

--- word-of-the-day.sh
+++ word-of-the-day.sh
@@ -3,9 +3,9 @@
 CURRENT_PATH="$(cd "$(dirname "$0")" && pwd -P)"
 WORDS_PATH="${CURRENT_PATH}/words"
 
-file=$(ls "${WORDS_PATH}" | sort -R | tail -n1)
+file=$(find "${WORDS_PATH}" -type f | sort -R | tail -n1)
 
 echo '--------------========|       WORD  OF  THE  DAY       |========--------------'
-cat "${WORDS_PATH}/${file}"
+cat "${file}"
 echo
 echo '--------------========|       WORD  OF  THE  DAY       |========--------------'
