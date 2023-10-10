119c80dd30b7319419da12fb29c26a6a76141621
null <null>
2023-10-10 19:18:18 +0200

    Do not repeat headers in each file

--- word-of-the-day.sh
+++ word-of-the-day.sh
@@ -3,9 +3,11 @@
 CURRENT_PATH="$(cd "$(dirname "$0")" && pwd -P)"
 WORDS_PATH="${CURRENT_PATH}/words"
 
-file=$(find "${WORDS_PATH}" -type f | sort -R | tail -n1)
+file=$(find "${WORDS_PATH}" -type f -iname '*.txt' | sort -R | tail -n1)
+header=$(dirname "${file}")"/lang.header"
 
 echo '--------------========|       WORD  OF  THE  DAY       |========--------------'
+cat "${header}"
+echo "-- ----- --"
 cat "${file}"
-echo
 echo '--------------========|       WORD  OF  THE  DAY       |========--------------'
