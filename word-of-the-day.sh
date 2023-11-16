86462305b247667968d2b3441bd2197cdeea151c
mwilczek-net <mwilczek-net@users.noreply.github.com>
2023-11-16 15:41:55 +0100

    Move headers form word-of-the-day to source file in sh-config repo

--- word-of-the-day.sh
+++ word-of-the-day.sh
@@ -6,8 +6,7 @@ WORDS_PATH="${CURRENT_PATH}/words"
 file=$(find "${WORDS_PATH}" -type f -iname '*.txt' | sort -R | tail -n1)
 header=$(dirname "${file}")"/lang.header"
 
-echo '--------------========|       WORD  OF  THE  DAY       |========--------------'
 cat "${header}"
-echo "-- ----- --"
+echo "-- ---- --"
 cat "${file}"
-echo '--------------========|       WORD  OF  THE  DAY       |========--------------'
+
